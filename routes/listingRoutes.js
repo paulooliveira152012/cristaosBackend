90053bf32e7220b1395fc9adf5a74a897a057d5f
paulooliveira152012 <paulooliveira152012@gmail.com>
2025-07-18 13:01:43 -0300

    work on notifications

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -19,6 +19,7 @@ router.get("/alllistings", async (req, res) => {
 
     res.status(200).json({ listings });
   } catch (error) {
+    console.log("error:", error)
     res.status(500).json({ message: "Error fetching listings", error });
   }
 });
