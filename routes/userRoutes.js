48a65be1172c95fdcd6f60d994c7950dd6101997
paulooliveira152012 <paulooliveira152012@gmail.com>
2025-07-19 16:13:03 -0300

    changing sameSite at logi from Lax, that means same site to None, since I'm using frontend and backend from different sources

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -165,7 +165,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true em produção
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
     });
 
